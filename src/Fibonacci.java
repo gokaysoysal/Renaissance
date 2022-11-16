ffe975795619130cd9a36191e09ba608e15932b1
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-16 20:43:46 +0300

    exercise

---
+++ src/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int number, n3;
+        int n1 = 0;
+        int n2 = 1;
+
+        System.out.print("Please enter a fibonacci number: ");
+        number = inp.nextInt();
+
+        System.out.print(n1 + " ");
+        System.out.print(n2 + " ");
+
+        for (int i = 2; i <= number; i++) {
+            n3 = n1 + n2;
+            System.out.print(n3 + " ");
+
+            n1 = n2;
+            n2 = n3;
+
+        }
+    }
+}

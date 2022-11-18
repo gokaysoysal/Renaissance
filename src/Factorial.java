ae6c4e83c052664bc1ecfa637b57cfe009f1d7a8
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-18 22:17:26 +0300

    exercise

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        int total = 1;
-
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Enter target factorial number: ");
-        int n = inp.nextInt();
+        int total = 1;
 
-        for (int i = 1; i <= n; i++) {
-            total = total * i;
+        System.out.print("Please enter factorial number: ");
+        int fac = inp.nextInt();
 
-
+        for (int i = 1; i <= fac; i++) {
+            total = (total * i);
         }
-        System.out.println(n+". Factorial is = " + total);
+        System.out.print(total);
     }
 }

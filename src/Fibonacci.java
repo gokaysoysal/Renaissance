b2d8e89ee4980362d50106c9db986583ffbc7e23
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-18 21:43:48 +0300

    exercise

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -15,7 +15,7 @@ public class Fibonacci {
         System.out.print(n2 + " ");
 
         for (int i = 2; i <= number; i++) {
-            n3 = n1 + n2;
+            n4 = n1 + n2;
             System.out.print(n4 + " ");
 
             n1 = n2;

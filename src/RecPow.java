63475bf17f183248e4158311decef9460da62ff1
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-21 22:09:17 +0300

    exercise

---
+++ src/RecPow.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RecPow {
+    static double power(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        System.out.println("Result: " + result);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Please enter base number: ");
+            int a = inp.nextInt();
+            System.out.print("Please enter exp number: ");
+            int b = inp.nextInt();
+
+            power(a, b);
+        }
+    }
+
+}
+

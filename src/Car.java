4417ba985ca1da1af0a8f1c3fdc18ec4e03f25ba
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-22 23:25:51 +0300

    exercise

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,37 @@
 class Car {
-    String type = "sedan";
+    String type;
     String model;
     String color;
     int speed;
-    int speedLimit = 200;
+    int speedLimit;
+
+    Car(String model, int speed, String color){
+        this.type = "Sedan";
+        this.model = model;
+        this.color = color;
+        this.speed = speed;
+        this.speedLimit = 200;
+    }
 
     void increaseSpeed (int increment) {
-        if((speed + increment)< speedLimit){
-            speed+=increment;
+        if((this.speed + increment)< this.speedLimit){
+            this.speed+=increment;
         }
     }
     void decreaseSpeed (int decrease){
-        if ((speed - decrease)>0){
-            speed-=decrease;
+        if ((this.speed - decrease)>0){
+            this.speed-=decrease;
         }
     }
 
     void printSpeed(){
-        System.out.println(model + " Speed: " + speed);
+        System.out.println(this.model + " Speed: " + this.speed);
+    }
+    void printInfo(){
+        System.out.println("Type\t: " + this.type);
+        System.out.println("Model\t: " + this.model);
+        System.out.println("Color\t: " + this.color);
+        System.out.println("Speed\t: " + this.speed);
+        System.out.println("===========================");
     }
 }

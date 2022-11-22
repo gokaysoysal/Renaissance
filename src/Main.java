6cc06ef586c9c555bcd6d7b8c86634278345a5a7
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-22 23:27:24 +0300

    exercise

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Car audi = new Car("Audi A7", 10, "Blue");
         audi.printInfo();
 
-        Car bmw = new Car("BMW X6", 30,"Green");
+        Car bmw = new Car("BMW X6", 30,"Red");
         bmw.printInfo();
 
         Car mercedes = new Car("Mercedes S600", 40, "Black");

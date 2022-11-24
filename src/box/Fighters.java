39bdd18cb7eb0c7ddf1e8ae26f8630110fd8fdac
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-24 22:42:37 +0300

    exercise

--- src/box/Fighters.java
+++ src/box/Fighters.java
@@ -5,20 +5,30 @@ public class Fighters {
     int damage;
     int health;
     int weight;
+    int dodge;
 
-    Fighters(String name, int damage, int health, int weight) {
+    Fighters(String name, int damage, int health, int weight, int dodge) {
         this.name = name;
         this.damage = damage;
         this.health = health;
         this.weight = weight;
-
+        this.dodge = dodge;
     }
 
     int hit(Fighters foe) {
-        System.out.println(this.name + "  ===>  " + foe.name + "  " + this.damage + " damage");
+        System.out.println(this.name + "  ===>  " + foe.name + "  " + this.damage + " received damage");
+        if (foe.isDodge()) {
+            System.out.println(foe.name + " is blocked attack!");
+            return foe.health;
+        }
         if (foe.health - this.damage <= 0) {
             return 0;
         }
         return foe.health - this.damage;
+    }
+
+    boolean isDodge() {
+        double randomNumber = Math.random() * 100;
+        return randomNumber <= this.dodge;
     }
 }

c71f8d5d7f3bdd0a859a5e250010d5b181fc6bd9
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-25 20:01:00 +0300

    exercise

--- src/box/Main.java
+++ src/box/Main.java
@@ -3,8 +3,8 @@ package box;
 public class Main {
     public static void main(String[] args) {
 
-        Fighters f1 = new Fighters("A", 25, 120, 100, 50);
-        Fighters f2 = new Fighters("B", 15, 105, 95, 70);
+        Fighters f1 = new Fighters("Jack", 25, 120, 100, 50);
+        Fighters f2 = new Fighters("Daniel", 15, 105, 95, 70);
 
         Match match = new Match(f1, f2, 80, 100);
         match.run();

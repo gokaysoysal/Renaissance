49b4721c949ba86269c2569b65b37a8e99783c72
Gökay Soysal <agokaysoysal@gmail.com>
2022-11-27 17:07:28 +0300

    exercise

---
+++ src/Polymorphism/BaseKrediManager.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class BaseKrediManager {
+    double hesapla(double tutar) {
+        return tutar * 1.18;
+    }
+}
---
+++ src/Polymorphism/OgrenciKrediManager.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class OgrenciKrediManager extends BaseKrediManager {
+    double hesapla(double tutar) {
+        return tutar * 1.10;
+    }
+}

7993db5610ff5927ea0b2a39c26d38883bd6478d
Gökay Soysal <agokaysoysal@gmail.com>
2022-12-01 22:05:59 +0300

    exercise

--- src/LetterP.java
+++ src/LetterP.java
@@ -2,21 +2,21 @@ public class LetterP {
     public static void main(String[] args) {
         String[][] matris = new String[6][3];
 
-        for (int i = 0; i<matris.length;i++){
-            for (int j = 0;j<matris[i].length;j++){
-                if (i==0||i==2){
-                    matris[i][j]=" * ";
-                } else if (j==0) {
-                    matris[i][j]=" * ";                    
-                } else if (i==1&&j==2) {
-                    matris[i][j]=" * ";
-                }else {
-                    matris[i][j]= "   ";
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+                if (i == 0 || i == 2) {
+                    matris[i][j] = " * ";
+                } else if (j == 0) {
+                    matris[i][j] = " * ";
+                } else if (i == 1 && j == 2) {
+                    matris[i][j] = " * ";
+                } else {
+                    matris[i][j] = "   ";
                 }
             }
         }
-        for (String[] row: matris){
-            for (String col : row){
+        for (String[] row : matris) {
+            for (String col : row) {
                 System.out.print(col);
             }
             System.out.println();

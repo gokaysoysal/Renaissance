9898ed0677a62aedee112058f132b63cb4e59127
Gökay Soysal <agokaysoysal@gmail.com>
2022-12-03 15:17:31 +0300

    exercise

---
+++ src/AbstractDemo/MySqLDatabaseManager.java
@@ -0,0 +1,8 @@
+package AbstractDemo;
+
+public class MySqLDatabaseManager extends BaseDatabaseManager {
+    @Override
+    public void getData() {
+        System.out.println("Veri getirildi: MySqL");
+    }
+}

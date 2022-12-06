4a27da6173af5c10ff87235175c9dd31003622b6
Gökay Soysal <agokaysoysal@gmail.com>
2022-12-06 22:45:27 +0300

    exercise

--- src/Transpose/Main.java
+++ src/Transpose/Main.java
@@ -3,8 +3,8 @@ package Transpose;
 public class Main {
     public static void main(String[] args) {
         int[][] list = {
-                {1, 2, 3},
-                {4, 5, 6}};
+                {3, 4, 5},
+                {6, 7, 8}};
         int[][] transpose = new int[list[0].length][list.length];
 
         System.out.println("Array: ");

03573648af2248fcfda39739240333525f993a8b
Gökay Soysal <agokaysoysal@gmail.com>
2022-12-13 22:46:04 +0300

    exercise

--- src/MineSweeper/Main.java
+++ src/MineSweeper/Main.java
@@ -1,8 +1,16 @@
 package MineSweeper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MineSweeper mineSweeper = new MineSweeper(4,4);
+        Scanner scan = new Scanner(System.in);
+        int row,col;
+        System.out.print("Please enter row number: ");
+        row = scan.nextInt();
+        System.out.print("Please enter col number: ");
+        col = scan.nextInt();
+        MineSweeper mineSweeper = new MineSweeper(row,col);
         mineSweeper.run();
 
     }
--- src/MineSweeper/MineSweeper.java
+++ src/MineSweeper/MineSweeper.java
@@ -8,6 +8,7 @@ public class MineSweeper {
     int rowNumb, colNumb, size;
     int[][] map;
     int[][] board;
+    boolean isWin = false;
 
     Random rand = new Random();
     Scanner scan = new Scanner(System.in);
@@ -21,12 +22,79 @@ public class MineSweeper {
 
     }
 
+    //oyun başlatma
     public void run() {
+        int row, col;
+        int finish = 0;
         prepareGame();
         print(map);
+        System.out.println("The game has start");
+        while (!isWin) {
+            print(board);
+            System.out.print("Select row coordinate: ");
+            row = scan.nextInt();
+            row = rowControl(row, scan);
+
+            System.out.print("Select column coordinate: ");
+            col = scan.nextInt();
+            col = colControl(col, scan);
+            if (map[row][col] != -1) {
+                check(row, col);
+                finish++;
+                if (finish - (size / 4) == 0) {
+                    System.out.println("Congratulations!");
+                }
+            } else {
+                isWin = false;
+                System.out.println("Game Over!");
+            }
+        }
 
     }
 
+    // satır kontrol
+    public int rowControl(int rowCont, Scanner scan) {
+        while ((rowCont >= this.rowNumb) || (rowCont < 0)) {
+            System.out.println("Please enter 0-" + this.rowNumb + ":\t");
+            rowCont = scan.nextInt();
+        }
+        return rowCont;
+    }
+
+    // sutün kontrol
+
+    public int colControl(int colCont, Scanner scan) {
+        while ((colCont >= this.colNumb) || (colCont < 0)) {
+            System.out.print("Plese enter 0-" + this.colNumb + ":\t");
+            colCont = scan.nextInt();
+        }
+        return colCont;
+    }
+
+    // mayın kontrol
+    public void check(int row, int col) {
+        if (map[row][col] == 0) {
+            if ((col < colNumb - 1) && (map[row][col + 1] == -1)) {
+                board[row][col]++;
+            }
+            if ((row < rowNumb - 1) && (map[row + 1][col] == -1)) {
+                board[row][col]++;
+            }
+            if ((col > 0) && (map[row][col - 1] == -1)) {
+                board[row][col]++;
+            }
+            if ((row > 0) && (map[row - 1][col] == -1)) {
+                board[row][col]++;
+            }
+            if (board[row][col] == 0) {
+                board[row][col] = 5;
+            }
+
+        }
+
+    }
+
+    //mayın yerleştirme
     public void prepareGame() {
         int randRow, randCol, count = 0;
         while (count <= (size / 4)) {
@@ -39,6 +107,7 @@ public class MineSweeper {
         }
     }
 
+    //map görünüm
     public void print(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
